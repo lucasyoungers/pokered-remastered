4374ed569b6efd25cdc685d62c150834b7b01a10
lucasyoungers <lucas005xl@gmail.com>
2022-12-19 00:07:43 -0800

    updated clefable line's level up moves and base stats

--- data/pokemon/evos_moves.asm
+++ data/pokemon/evos_moves.asm
@@ -236,12 +236,13 @@ ClefairyEvosMoves:
 	db EV_ITEM, MOON_STONE, 1, CLEFABLE
 	db 0
 ; Learnset
-	db 13, SING
-	db 18, DOUBLESLAP
-	db 24, MINIMIZE
-	db 31, METRONOME
-	db 39, DEFENSE_CURL
-	db 48, LIGHT_SCREEN
+	db 9, SING
+	db 13, DOUBLESLAP
+	db 21, MINIMIZE
+	db 25, DEFENSE_CURL
+	db 29, METRONOME
+	db 37, RECOVER
+	db 41, LIGHT_SCREEN
 	db 0
 
 SpearowEvosMoves:
