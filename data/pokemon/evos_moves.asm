746ca492b2a0d4467f00041747fa35a2f8f8d3dc
lucasyoungers <lucas005xl@gmail.com>
2022-12-19 11:31:54 -0800

    updated tentacruel line's level up moves and base stats

--- data/pokemon/evos_moves.asm
+++ data/pokemon/evos_moves.asm
@@ -465,15 +465,15 @@ TentacoolEvosMoves:
 	db EV_LEVEL, 30, TENTACRUEL
 	db 0
 ; Learnset
-	db 7, SUPERSONIC
-	db 13, WRAP
-	db 18, POISON_STING
-	db 22, WATER_GUN
-	db 27, CONSTRICT
-	db 33, BARRIER
-	db 40, SCREECH
-	db 48, HYDRO_PUMP
-	db 0
+	db 6, SUPERSONIC
+	db 12, CONSTRICT
+	db 19, ACID
+	db 25, BUBBLEBEAM
+	db 30, WRAP
+	db 36, ACID_ARMOR
+	db 43, SCREECH
+	db 49, SLUDGE_WAVE
+	db 55, HYDRO_PUMP
 
 GastlyEvosMoves:
 ; Evolutions
@@ -1874,14 +1874,15 @@ TentacruelEvosMoves:
 ; Evolutions
 	db 0
 ; Learnset
-	db 7, SUPERSONIC
-	db 13, WRAP
-	db 18, POISON_STING
-	db 22, WATER_GUN
-	db 27, CONSTRICT
-	db 35, BARRIER
-	db 43, SCREECH
-	db 50, HYDRO_PUMP
+	db 6, SUPERSONIC
+	db 12, CONSTRICT
+	db 19, ACID
+	db 25, BUBBLEBEAM
+	db 30, WRAP
+	db 38, ACID_ARMOR
+	db 47, SCREECH
+	db 55, SLUDGE_WAVE
+	db 63, HYDRO_PUMP
 	db 0
 
 MissingNo9CEvosMoves:
