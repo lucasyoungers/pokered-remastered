276b6154bed7208638134729953fa135464a51d3
lucasyoungers <lucas005xl@gmail.com>
2022-12-19 00:17:39 -0800

    updated wigglytuff line's level up moves and base stats

--- data/pokemon/evos_moves.asm
+++ data/pokemon/evos_moves.asm
@@ -1257,12 +1257,13 @@ JigglypuffEvosMoves:
 	db EV_ITEM, MOON_STONE, 1, WIGGLYTUFF
 	db 0
 ; Learnset
+	db 4, DEFENSE_CURL
 	db 9, POUND
 	db 14, DISABLE
-	db 19, DEFENSE_CURL
-	db 24, DOUBLESLAP
-	db 29, REST
-	db 34, BODY_SLAM
+	db 19, DOUBLESLAP
+	db 24, REST
+	db 29, BODY_SLAM
+	db 34, MIMIC
 	db 39, DOUBLE_EDGE
 	db 0
 
