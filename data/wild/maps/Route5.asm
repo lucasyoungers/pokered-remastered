74f47e2afaaa789093c61cf15c74cb555917c210
lucasyoungers <lucas005xl@gmail.com>
2022-12-25 14:36:01 -0800

    updated route 5, both fires now available in both versions

--- data/wild/maps/Route5.asm
+++ data/wild/maps/Route5.asm
@@ -1,30 +1,36 @@
 Route5WildMons:
 	def_grass_wildmons 15 ; encounter rate
 IF DEF(_RED)
-	db 13, ODDISH
-	db 13, PIDGEY
-	db 15, PIDGEY
-	db 10, MANKEY
-	db 12, MANKEY
-	db 15, ODDISH
-	db 16, ODDISH
-	db 16, PIDGEY
-	db 14, MANKEY
-	db 16, MANKEY
+	db 13, GROWLITHE
+	db 14, PSYDUCK
+	db 14, JIGGLYPUFF
+	db 15, JIGGLYPUFF
+	db 14, GROWLITHE
+	db 16, GROWLITHE
+	db 13, ABRA
+	db 15, ABRA
+	db 12, VULPIX
+	db 13, VULPIX
 ENDC
 IF DEF(_BLUE)
-	db 13, BELLSPROUT
-	db 13, PIDGEY
-	db 15, PIDGEY
-	db 10, MEOWTH
-	db 12, MEOWTH
-	db 15, BELLSPROUT
-	db 16, BELLSPROUT
-	db 16, PIDGEY
-	db 14, MEOWTH
-	db 16, MEOWTH
+	db 13, VULPIX
+	db 14, PSYDUCK
+	db 14, JIGGLYPUFF
+	db 15, JIGGLYPUFF
+	db 14, VULPIX
+	db 16, VULPIX
+	db 13, ABRA
+	db 15, ABRA
+	db 12, GROWLITHE
+	db 13, GROWLITHE
 ENDC
 	end_grass_wildmons
 
 	def_water_wildmons 0 ; encounter rate
 	end_water_wildmons
+
+; 40% : Fire1      : 13-16
+; 25% : Jigglypuff : 14-15
+; 20% : Psyduck    : 14
+; 10% : Abra       : 13-15
+; 5%  : Fire2      : 12-13
