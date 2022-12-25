73a89dd8d23c207a5ef4ac4476605cdd81d016f5
lucasyoungers <lucas005xl@gmail.com>
2022-12-25 14:23:33 -0800

    updated route 25 wild encounters; added venonat to route, and made eevee obtainable as a 1% encounter here

--- data/wild/maps/Route25.asm
+++ data/wild/maps/Route25.asm
@@ -1,30 +1,35 @@
 Route25WildMons:
 	def_grass_wildmons 15 ; encounter rate
 IF DEF(_RED)
-	db  8, WEEDLE
-	db  9, KAKUNA
-	db 13, PIDGEY
 	db 12, ODDISH
 	db 13, ODDISH
-	db 12, ABRA
+	db 12, VENONAT
+	db 13, VENONAT
+	db 14, VENONAT
 	db 14, ODDISH
-	db 10, ABRA
-	db  7, METAPOD
-	db  8, CATERPIE
+	db 12, ABRA
+	db 13, ABRA
+	db 12, BELLSPROUT
 ENDC
 IF DEF(_BLUE)
-	db  8, CATERPIE
-	db  9, METAPOD
-	db 13, PIDGEY
 	db 12, BELLSPROUT
 	db 13, BELLSPROUT
-	db 12, ABRA
+	db 12, VENONAT
+	db 13, VENONAT
+	db 14, VENONAT
 	db 14, BELLSPROUT
-	db 10, ABRA
-	db  7, KAKUNA
-	db  8, WEEDLE
+	db 12, ABRA
+	db 13, ABRA
+	db 12, ODDISH
 ENDC
+	db 10, EEVEE
 	end_grass_wildmons
 
 	def_water_wildmons 0 ; encounter rate
 	end_water_wildmons
+
+; 50% : Grass1  : 12-14
+; 35% : Venonat : 12-14
+; 10% : Abra    : 12-13
+; 4%  : Grass2  : 12
+; 1%  : Eevee   : 10
