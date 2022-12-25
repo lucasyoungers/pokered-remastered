9bd76ad2efdf9f4c87b20a31bb6442fafbf2920f
lucasyoungers <lucas005xl@gmail.com>
2022-12-25 13:54:17 -0800

    updated route 4 encounters, added mankey and meowth

--- data/wild/maps/Route4.asm
+++ data/wild/maps/Route4.asm
@@ -1,27 +1,32 @@
 Route4WildMons:
 	def_grass_wildmons 20 ; encounter rate
-	db 10, RATTATA
 	db 10, SPEAROW
-	db  8, RATTATA
+	db 11, SPEAROW
 IF DEF(_RED)
-	db  6, EKANS
-	db  8, SPEAROW
-	db 10, EKANS
-	db 12, RATTATA
-	db 12, SPEAROW
-	db  8, EKANS
+	db 11, MANKEY
+	db 10, MANKEY
+	db 12, MANKEY
+	db 11, EKANS
 	db 12, EKANS
+	db  9, MEOWTH
 ENDC
 IF DEF(_BLUE)
-	db  6, SANDSHREW
-	db  8, SPEAROW
-	db 10, SANDSHREW
-	db 12, RATTATA
-	db 12, SPEAROW
-	db  8, SANDSHREW
+	db 11, MEOWTH
+	db 10, MEOWTH
+	db 12, MEOWTH
+	db 11, SANDSHREW
 	db 12, SANDSHREW
+	db  9, MANKEY
 ENDC
+	db 10, LICKITUNG
+	db 12, LICKITUNG
 	end_grass_wildmons
 
 	def_water_wildmons 0 ; encounter rate
 	end_water_wildmons
+
+; 40% : Spearow   : 10-11
+; 35% : Mankey/Meowth   : 10-12
+; 15% ; Ekans/Sandshrew : 11-12
+; 5%  : Meowth/Mankey   : 9
+; 5%  : Lickitung : 10-12
