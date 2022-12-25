4aaf986946344b2e01261c9a8593a855dbb90487
lucasyoungers <lucas005xl@gmail.com>
2022-12-25 12:08:16 -0800

    updated mount moon, lessened focus on zubat and added rare onix spawn

--- data/wild/maps/MtMoonB1F.asm
+++ data/wild/maps/MtMoonB1F.asm
@@ -1,16 +1,21 @@
 MtMoonB1FWildMons:
 	def_grass_wildmons 10 ; encounter rate
 	db  8, ZUBAT
-	db  7, ZUBAT
-	db  7, GEODUDE
-	db  8, GEODUDE
+	db 10, GEODUDE
+	db 12, GEODUDE
 	db  9, ZUBAT
-	db 10, PARAS
-	db 10, ZUBAT
 	db 11, ZUBAT
-	db  9, CLEFAIRY
-	db  9, GEODUDE
+	db 10, PARAS
+	db  9, PARAS
+	db 11, PARAS
+	db 11, CLEFAIRY
+	db 12, CLEFAIRY
 	end_grass_wildmons
 
 	def_water_wildmons 0 ; encounter rate
 	end_water_wildmons
+
+; 40% : Zubat    : 8-11
+; 35% : Geodude  : 10-12
+; 20% : Paras    : 9-11
+; 5%  : Clefairy : 11-12
--- data/wild/maps/MtMoonB2F.asm
+++ data/wild/maps/MtMoonB2F.asm
@@ -1,16 +1,22 @@
 MtMoonB2FWildMons:
 	def_grass_wildmons 10 ; encounter rate
-	db  9, ZUBAT
-	db  9, GEODUDE
-	db 10, ZUBAT
-	db 10, GEODUDE
 	db 11, ZUBAT
+	db 10, GEODUDE
+	db 11, GEODUDE
+	db 12, GEODUDE
 	db 10, PARAS
 	db 12, PARAS
-	db 10, CLEFAIRY
-	db 12, ZUBAT
-	db 12, CLEFAIRY
+	db 11, CLEFAIRY
+	db 13, CLEFAIRY
+	db 13, ZUBAT
+	db 15, ONIX
 	end_grass_wildmons
 
 	def_water_wildmons 0 ; encounter rate
 	end_water_wildmons
+
+; 45% : Geodude  : 10-12
+; 24% : Zubat    : 11-13
+; 20% : Paras    : 10-12
+; 10% : Clefairy : 11-13
+; 1%  : Onix     : 15
