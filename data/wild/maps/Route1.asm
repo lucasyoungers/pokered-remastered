0099b6b3d7a5152312d20c5c131d43b5f2723d80
lucasyoungers <lucas005xl@gmail.com>
2022-12-25 11:16:42 -0800

    updated route 1 encounters, added tangela

--- data/wild/maps/Route1.asm
+++ data/wild/maps/Route1.asm
@@ -6,11 +6,15 @@ Route1WildMons:
 	db  2, RATTATA
 	db  2, PIDGEY
 	db  3, PIDGEY
-	db  3, PIDGEY
-	db  4, RATTATA
 	db  4, PIDGEY
-	db  5, PIDGEY
+	db  4, RATTATA
+	db  3, TANGELA
+	db  4, TANGELA
 	end_grass_wildmons
 
 	def_water_wildmons 0 ; encounter rate
 	end_water_wildmons
+
+; 50% : Rattata	: 2-4
+; 45% : Pidgey  : 2-4
+; 5%  : Tangela : 3-4
