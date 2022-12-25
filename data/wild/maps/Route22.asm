310c9166abb2c5f91f7aabcdb37ca687e363e42b
lucasyoungers <lucas005xl@gmail.com>
2022-12-25 11:17:27 -0800

    updated route 22 encounters, made spearow and nidorans more common

--- data/wild/maps/Route22.asm
+++ data/wild/maps/Route22.asm
@@ -1,29 +1,32 @@
 Route22WildMons:
 	def_grass_wildmons 25 ; encounter rate
+	db  4, SPEAROW
 	db  3, RATTATA
-IF DEF(_RED)
-	db  3, NIDORAN_M
 	db  4, RATTATA
+IF DEF(_RED)
+	db  5, NIDORAN_M
 	db  4, NIDORAN_M
-	db  2, RATTATA
+	db  3, NIDORAN_M
 	db  2, NIDORAN_M
-	db  3, SPEAROW
-	db  5, SPEAROW
 	db  3, NIDORAN_F
 	db  4, NIDORAN_F
+	db  5, NIDORAN_F
 ENDC
 IF DEF(_BLUE)
-	db  3, NIDORAN_F
-	db  4, RATTATA
+	db  5, NIDORAN_F
 	db  4, NIDORAN_F
-	db  2, RATTATA
+	db  3, NIDORAN_F
 	db  2, NIDORAN_F
-	db  3, SPEAROW
-	db  5, SPEAROW
 	db  3, NIDORAN_M
 	db  4, NIDORAN_M
+	db  5, NIDORAN_M
 ENDC
 	end_grass_wildmons
 
 	def_water_wildmons 0 ; encounter rate
 	end_water_wildmons
+
+; 35% : Rattata  : 3-4
+; 20% : Spearow  : 4
+; 35% : Nidoran1 : 2-5
+; 10% : Nidoran2 : 3-5
