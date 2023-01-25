5fdbc60bb6f7670d9144f582237c42373795bc17
lucasyoungers <lucas005xl@gmail.com>
2023-01-24 20:33:19 -0800

    updated route 18 wild encounters, mostly buffed levels

--- data/wild/maps/Route18.asm
+++ data/wild/maps/Route18.asm
@@ -1,15 +1,15 @@
 Route18WildMons:
 	def_grass_wildmons 25 ; encounter rate
-	db 20, SPEAROW
-	db 22, SPEAROW
-	db 25, RATICATE
-	db 24, DODUO
-	db 25, FEAROW
-	db 26, DODUO
 	db 28, DODUO
-	db 29, RATICATE
-	db 27, FEAROW
-	db 29, FEAROW
+	db 26, SPEAROW
+	db 27, SPEAROW
+	db 28, SPEAROW
+	db 30, FARFETCHD
+	db 30, DODUO
+	db 32, DODRIO
+	db 31, FEAROW
+	db 33, FEAROW
+	db 35, FEAROW
 	end_grass_wildmons
 
 	def_water_wildmons 0 ; encounter rate
