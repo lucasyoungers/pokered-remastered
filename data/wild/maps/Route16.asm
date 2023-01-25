e9ff5ffdb1759f5424f8751579b27b244cf166da
lucasyoungers <lucas005xl@gmail.com>
2023-01-24 18:57:29 -0800

    updated route 16's wild encounters to be more bird focused, and added version poisons

--- data/wild/maps/Route16.asm
+++ data/wild/maps/Route16.asm
@@ -1,15 +1,21 @@
 Route16WildMons:
 	def_grass_wildmons 25 ; encounter rate
-	db 20, SPEAROW
 	db 22, SPEAROW
-	db 18, RATTATA
-	db 20, DODUO
-	db 20, RATTATA
-	db 18, DODUO
 	db 22, DODUO
-	db 22, RATTATA
-	db 23, RATICATE
-	db 25, RATICATE
+	db 23, DODUO
+	db 25, DODUO
+	db 24, SPEAROW
+IF DEF(_RED)
+	db 20, KOFFING
+	db 22, KOFFING
+ENDC
+IF DEF(_BLUE)
+	db 20, GRIMER
+	db 22, GRIMER
+ENDC
+	db 27, FEAROW
+	db 31, DODRIO
+	db 33, DODRIO
 	end_grass_wildmons
 
 	def_water_wildmons 0 ; encounter rate
