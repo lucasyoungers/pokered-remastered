33c22cb6730ceb67ae3debac39b7d8df19869e3f
lucasyoungers <lucas005xl@gmail.com>
2023-01-24 17:15:56 -0800

    updated route 11 wild encounters, focusing more on magnemite and drowzee, also added rare mr. mime and magneton encounters

--- data/wild/maps/Route11.asm
+++ data/wild/maps/Route11.asm
@@ -1,26 +1,23 @@
 Route11WildMons:
 	def_grass_wildmons 15 ; encounter rate
+	db 18, MAGNEMITE
+	db 17, DROWZEE
+	db 19, DROWZEE
+	db 20, DROWZEE
+	db 20, MAGNEMITE
+
 IF DEF(_RED)
-	db 14, EKANS
-	db 15, SPEAROW
-	db 12, EKANS
-	db  9, DROWZEE
-	db 13, SPEAROW
-	db 13, DROWZEE
-	db 15, EKANS
+	db 17, EKANS
+	db 18, EKANS
+	db 19, EKANS
 ENDC
 IF DEF(_BLUE)
-	db 14, SANDSHREW
-	db 15, SPEAROW
-	db 12, SANDSHREW
-	db  9, DROWZEE
-	db 13, SPEAROW
-	db 13, DROWZEE
-	db 15, SANDSHREW
+	db 17, SANDSHREW
+	db 18, SANDSHREW
+	db 19, SANDSHREW
 ENDC
-	db 17, SPEAROW
-	db 11, DROWZEE
-	db 15, DROWZEE
+	db 25, MR_MIME
+	db 30, MAGNETON
 	end_grass_wildmons
 
 	def_water_wildmons 0 ; encounter rate
