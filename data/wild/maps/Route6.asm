317ad86989cdd5077bf3c65fde08f5383073cf14
lucasyoungers <lucas005xl@gmail.com>
2023-01-24 17:07:13 -0800

    updated route 6 wild encounters, added psyduck and magnemite to both versions

--- data/wild/maps/Route6.asm
+++ data/wild/maps/Route6.asm
@@ -1,29 +1,24 @@
 Route6WildMons:
 	def_grass_wildmons 15 ; encounter rate
 IF DEF(_RED)
-	db 13, ODDISH
-	db 13, PIDGEY
-	db 15, PIDGEY
-	db 10, MANKEY
-	db 12, MANKEY
-	db 15, ODDISH
-	db 16, ODDISH
-	db 16, PIDGEY
 	db 14, MANKEY
+	db 14, ODDISH
+	db 15, ODDISH
+	db 12, MANKEY
 	db 16, MANKEY
 ENDC
 IF DEF(_BLUE)
-	db 13, BELLSPROUT
-	db 13, PIDGEY
-	db 15, PIDGEY
-	db 10, MEOWTH
-	db 12, MEOWTH
-	db 15, BELLSPROUT
-	db 16, BELLSPROUT
-	db 16, PIDGEY
 	db 14, MEOWTH
+	db 14, BELLSPROUT
+	db 15, BELLSPROUT
+	db 12, MEOWTH
 	db 16, MEOWTH
 ENDC
+	db 14, PSYDUCK
+	db 16, PSYDUCK
+	db 15, MAGNEMITE
+	db 16, MAGNEMITE
+	db 18, MAGNEMITE
 	end_grass_wildmons
 
 	def_water_wildmons 0 ; encounter rate
