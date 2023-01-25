c4f82bcba2e683dae4af13328910bd4f2007756b
lucasyoungers <lucas005xl@gmail.com>
2023-01-24 19:34:57 -0800

    updated route 17 wild encounters with more ponyta

--- data/wild/maps/Route17.asm
+++ data/wild/maps/Route17.asm
@@ -1,15 +1,15 @@
 Route17WildMons:
 	def_grass_wildmons 25 ; encounter rate
-	db 20, SPEAROW
-	db 22, SPEAROW
-	db 25, RATICATE
-	db 24, DODUO
-	db 27, RATICATE
-	db 26, DODUO
-	db 28, DODUO
-	db 29, RATICATE
-	db 25, FEAROW
-	db 27, FEAROW
+	db 23, DODUO
+	db 24, PONYTA
+	db 25, PONYTA
+	db 26, PONYTA
+	db 25, DODUO
+	db 25, PSYDUCK
+	db 30, EEVEE
+	db 32, DODRIO
+	db 40, RAPIDASH
+	db 40, RAPIDASH
 	end_grass_wildmons
 
 	def_water_wildmons 0 ; encounter rate
