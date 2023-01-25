c56340c623ca28b298e5f0ca6ea0bea979adad1a
lucasyoungers <lucas005xl@gmail.com>
2023-01-24 18:47:48 -0800

    updated route 7's wild encounters to reflect grasss more, added porygon as 1 percent encounter

--- data/wild/maps/Route7.asm
+++ data/wild/maps/Route7.asm
@@ -1,28 +1,27 @@
 Route7WildMons:
 	def_grass_wildmons 15 ; encounter rate
-	db 19, PIDGEY
+	db 20, JIGGLYPUFF
 IF DEF(_RED)
-	db 19, ODDISH
-	db 17, MANKEY
-	db 22, ODDISH
-	db 22, PIDGEY
-	db 18, MANKEY
-	db 18, GROWLITHE
-	db 20, GROWLITHE
-	db 19, MANKEY
-	db 20, MANKEY
+	db 21, ODDISH
+	db 23, ODDISH
+	db 22, GROWLITHE
+	db 24, GROWLITHE
+	db 22, GLOOM
+	db 25, GLOOM
+	db 22, JIGGLYPUFF
+	db 32, VILEPLUME
 ENDC
 IF DEF(_BLUE)
-	db 19, BELLSPROUT
-	db 17, MEOWTH
-	db 22, BELLSPROUT
-	db 22, PIDGEY
-	db 18, MEOWTH
-	db 18, VULPIX
-	db 20, VULPIX
-	db 19, MEOWTH
-	db 20, MEOWTH
+	db 21, BELLSPROUT
+	db 23, BELLSPROUT
+	db 22, VULPIX
+	db 24, VULPIX
+	db 22, WEEPINBELL
+	db 25, WEEPINBELL
+	db 22, JIGGLYPUFF
+	db 32, VICTREEBEL
 ENDC
+	db 25, PORYGON
 	end_grass_wildmons
 
 	def_water_wildmons 0 ; encounter rate
