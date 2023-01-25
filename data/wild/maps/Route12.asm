bcfb1b3afee812bac388f1f8426aa87473ac0e98
lucasyoungers <lucas005xl@gmail.com>
2023-01-24 19:06:30 -0800

    updated route 12 wild encounters, more focus on venonat, added kingler and farfetchd

--- data/wild/maps/Route12.asm
+++ data/wild/maps/Route12.asm
@@ -1,29 +1,15 @@
 Route12WildMons:
 	def_grass_wildmons 15 ; encounter rate
-IF DEF(_RED)
-	db 24, ODDISH
-	db 25, PIDGEY
-	db 23, PIDGEY
-	db 24, VENONAT
-	db 22, ODDISH
-	db 26, VENONAT
-	db 26, ODDISH
-	db 27, PIDGEY
-	db 28, GLOOM
-	db 30, GLOOM
-ENDC
-IF DEF(_BLUE)
-	db 24, BELLSPROUT
-	db 25, PIDGEY
-	db 23, PIDGEY
-	db 24, VENONAT
-	db 22, BELLSPROUT
-	db 26, VENONAT
-	db 26, BELLSPROUT
-	db 27, PIDGEY
-	db 28, WEEPINBELL
-	db 30, WEEPINBELL
-ENDC
+	db 22, KRABBY
+	db 22, VENONAT
+	db 23, VENONAT
+	db 25, VENONAT
+	db 24, KRABBY
+	db 25, FARFETCHD
+	db 28, KINGLER
+	db 30, KINGLER
+	db 31, VENOMOTH
+	db 33, VENOMOTH
 	end_grass_wildmons
 
 	def_water_wildmons 0 ; encounter rate
